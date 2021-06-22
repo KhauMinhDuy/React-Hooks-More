333a8304868645952927eb05b5c0bb6e7dff37c9
khauminhduy <khauminhduy@gmail.com>
2021-06-22 21:01:40 +0700

    update

---
+++ src/SpeakerDetail.js
@@ -0,0 +1,32 @@
+import ImageToggleOnScroll from './ImageToggleOnScroll';
+
+const SpeakerDetail = (props) => {
+  const {id,firstName,lastName,favorite,bio,onHeartFavoriteHandler} = props;
+  return (
+    <div className="card col-4 cardmin">
+      <ImageToggleOnScroll
+        // className="card-img-top"
+        primaryImg={`/static/speakers/bw/Speaker-${id}.jpg`}
+        secondaryImg={`/static/speakers/Speaker-${id}.jpg`}
+        // alt={`${firstName}  ${lastName}`}
+      />
+      <div className="card-body">
+        <h4 className="card-title">
+          <button
+            data-sessionid={id}
+            className={favorite ? 'heartredbutton' : 'heartdarkbutton'}
+            onClick={(e) => {
+              onHeartFavoriteHandler(e, !favorite);
+            }}
+          />
+          <span>
+            {firstName} {lastName}
+          </span>
+        </h4>
+        <span>{bio}</span>
+      </div>
+    </div>
+  );
+};
+
+export default SpeakerDetail;
--- src/Speakers.js
+++ src/Speakers.js
@@ -1,9 +1,62 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Header from './Header';
+import Menu from './Menu';
+import speakerData from './SpeakerData';
+import SpeakerDetail from './SpeakerDetail';
 
 const Speakers = (props) => {
+
+  const [speakerList, setSpeakerList] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const resolve = () => {
+
+  }
+
+  useEffect(() => {
+    setIsLoading(true);
+    new Promise(() => {
+      setTimeout(() => {
+        resolve();
+      }, 1000);
+    }).then(() => {
+      setIsLoading(false);
+    });
+    setSpeakerList([]);
+    return () => {
+      console.log(`cleanup`);
+    }
+  }, []);
+
+  const heartFavoriteHandler = (e, favorite) => {
+    e.preventDefault();
+  }
+
+  if(isLoading) return <div>Loading...</div>
+
   return (
-    <div>Speakers</div>
+    <div>
+      <Header />
+      <Menu />
+      <div className="container">
+        <div className="row">
+          <div className="card-deck">
+            {speakerData.map(({id, firstName, lastName, bio, favorite}) => {
+              return (
+                <SpeakerDetail 
+                  id={id} 
+                  firstName={firstName} lastName={lastName}
+                  bio={bio} favorite={favorite}  
+                  onHeartFavoriteHandler={heartFavoriteHandler}
+                />
+              )
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
 export default Speakers;
+
